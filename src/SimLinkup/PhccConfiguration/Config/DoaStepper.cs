bb20fef11d810662a7a53a0225a19cda43b001d6
José Saraiva <jsaraiva@zonmail.pt>
2018-06-30 21:05:56 +0100

    Refactored PHCC Stepper home-in signals to the PHCC config classes. Perhaps implementing a "setup" method on the base abstract class, and then call that method on every devices after phcc statup?

--- src/SimLinkup/PhccConfiguration/Config/DoaStepper.cs
+++ src/SimLinkup/PhccConfiguration/Config/DoaStepper.cs
@@ -1,13 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.MacroProgramming;
+using log4net;
 using PhccConfiguration.Config.ConfigClasses;
+using p = Phcc;
 
 namespace PhccConfiguration.Config
 {
     [Serializable]
     public class DoaStepper : Peripheral
     {
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(DoaStepper));
         public List<OutputConfig> OutputConfigs { get; set; } = new List<OutputConfig>();
         public List<HomingSignalConfig> HomingSignals { get; set; } = new List<HomingSignalConfig>();
+
+        #region Device specific methods
+
+        /// <summary>
+        ///Method to initialize the DOA steppers card if a "Home-In" switch is defined. 
+        /// </summary>
+        public bool HomeInStepper(AnalogSignal[] analogOutputs, DigitalSignal[] digitalInputs)
+        {
+
+
+            var analogSteppers = analogOutputs.Where(x => x.Id.Contains("DOA_STEPPER"))
+                .Where(x => HomingSignals.Any(y => y.MotorSignalId.Equals(x.Id))).ToList();
+
+            Parallel.ForEach(analogSteppers, (stepper) =>
+            {
+                var homingSignal = HomingSignals.FirstOrDefault(x => x.MotorSignalId == stepper.Id);
+                var digitalSignal = digitalInputs.FirstOrDefault(x => homingSignal != null && x.Id == homingSignal.ControlSignalId);
+
+                while (digitalSignal != null && digitalSignal.State)
+                {
+                    var source = stepper as CalibratedAnalogSignal;
+                    if (source?.Index == null) return;
+                    var motorNumZeroBase = source.Index.Value;
+                    var baseAddress = source.SubSourceAddress;
+                    var baseAddressByte = Convert.ToByte(baseAddress.Replace("0x", ""), 16);
+                    var device = source.Source as p.Device;
+                    if (device == null) return;
+                    var motorNum = motorNumZeroBase + 1;
+                    var direction = p.MotorDirections.Counterclockwise;
+
+                    try
+                    {
+                        device.DoaSendStepperMotor(baseAddressByte, (byte)motorNum, direction, 1,
+                            p.MotorStepTypes.FullStep);
+                        Thread.Sleep(20);
+                    }
+                    catch (Exception e)
+                    {
+
+                        _log.Error(e.Message, e);
+                        return;
+                    }
+#if DEBUG
+                    digitalSignal.State = !digitalSignal.State;
+#endif
+                }
+            });
+
+            return true;
+
+        }
+
+        #endregion
+
+
     }
 }
